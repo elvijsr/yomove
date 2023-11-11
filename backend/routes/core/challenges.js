b63bdfb8b10820767933036a39a66e45620e56e7
elvijsr <erascevs@gmail.com>
2023-11-11 21:00:41 +0200

    submit challenge endpoint and service

--- backend/routes/core/challenges.js
+++ backend/routes/core/challenges.js
@@ -15,4 +15,51 @@ router.get("/challenges", async (ctx, next) => {
   }
 });
 
+router.post("/submit-results", async (ctx) => {
+  try {
+    // Extracting the username from the header
+    const username = ctx.get("X-Username");
+    const { challenge_id, lobby_id, score } = ctx.request.body;
+
+    // Validate incoming data
+    if (
+      !username ||
+      challenge_id === undefined ||
+      lobby_id === undefined ||
+      score === undefined
+    ) {
+      ctx.status = 400; // Bad Request
+      ctx.body = { error: "Missing required fields" };
+      return;
+    }
+
+    // Look up the user ID based on the username
+    const user = await db.oneOrNone(
+      "SELECT id FROM users WHERE username = $1",
+      [username]
+    );
+
+    // If user not found, return an error
+    if (!user) {
+      ctx.status = 404; // Not Found
+      ctx.body = { error: "User not found" };
+      return;
+    }
+
+    // Insert the result into the database with the user's ID
+    await db.none(
+      "INSERT INTO results (user_id, challenge_id, lobby_id, score) VALUES ($1, $2, $3, $4)",
+      [user.id, challenge_id, lobby_id, score]
+    );
+
+    ctx.status = 201; // Created
+    ctx.body = { message: "Result submitted successfully" };
+  } catch (error) {
+    // Error handling
+    console.error(error);
+    ctx.status = 500; // Internal Server Error
+    ctx.body = { error: "Internal server error" };
+  }
+});
+
 module.exports = router;
