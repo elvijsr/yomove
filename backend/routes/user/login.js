788c8030af38bce0004286d04c46f1a1e2f19f44
elvijsr <erascevs@gmail.com>
2023-11-11 14:03:10 +0200

    check for empty username

--- backend/routes/user/login.js
+++ backend/routes/user/login.js
@@ -8,11 +8,18 @@ const returnSuccessOnExistingUser = true;
 router.post("/login", async (ctx) => {
   const { username } = ctx.request.body;
 
+  // Check if username is provided and not empty
+  if (!username || username.trim() === "") {
+    ctx.status = 400;
+    ctx.body = { error: "Username cannot be empty" };
+    return;
+  }
+
   try {
     // Check if the user already exists
     const userExists = await db.oneOrNone(
       "SELECT username FROM users WHERE username = $1",
-      username
+      username.trim()
     );
 
     if (userExists) {
@@ -25,10 +32,18 @@ router.post("/login", async (ctx) => {
         ctx.body = { error: "Username already exists" };
       }
     } else {
-      // Insert the new username into the users table
-      await db.none("INSERT INTO users(username) VALUES($1)", username);
+      // Fetch a random avatar_id
+      const avatar = await db.one(
+        "SELECT id FROM pictures WHERE type = 0 ORDER BY RANDOM() LIMIT 1"
+      );
+
+      // Insert the new username into the users table with the avatar_id
+      await db.none("INSERT INTO users(username, avatar_id) VALUES($1, $2)", [
+        username.trim(),
+        avatar.id,
+      ]);
       ctx.status = 201; // Created
-      ctx.body = { message: "User created successfully" };
+      ctx.body = { message: "User created successfully with avatar" };
     }
   } catch (error) {
     ctx.status = 500; // Internal Server Error
