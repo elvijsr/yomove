f03b7698a0c66160d9860a2d37b70517ca6e0407
elvijsr <erascevs@gmail.com>
2023-11-11 14:54:15 +0200

    adding avatar to user upon registration

--- backend/routes/user/login.js
+++ backend/routes/user/login.js
@@ -10,19 +10,19 @@ router.post("/login", async (ctx) => {
 
   // Check if username is provided and not empty
   if (!username || username.trim() === "") {
-    ctx.status = 400;
+    ctx.status = 400; // Bad Request
     ctx.body = { error: "Username cannot be empty" };
-    return;
+    return; // Stop further execution
   }
 
   try {
     // Check if the user already exists
-    const userExists = await db.oneOrNone(
-      "SELECT username FROM users WHERE username = $1",
+    const user = await db.oneOrNone(
+      "SELECT id FROM users WHERE username = $1",
       username.trim()
     );
 
-    if (userExists) {
+    if (user) {
       // If returnSuccessOnExistingUser is true, return a success message
       if (returnSuccessOnExistingUser) {
         ctx.status = 200; // OK
@@ -37,17 +37,37 @@ router.post("/login", async (ctx) => {
         "SELECT id FROM pictures WHERE type = 0 ORDER BY RANDOM() LIMIT 1"
       );
 
-      // Insert the new username into the users table with the avatar_id
-      await db.none("INSERT INTO users(username, avatar_id) VALUES($1, $2)", [
-        username.trim(),
-        avatar.id,
-      ]);
+      // Start a transaction
+      await db.tx(async (t) => {
+        // Insert the new username into the users table with the avatar_id
+        const insertUser = t.one(
+          "INSERT INTO users(username, avatar_id) VALUES($1, $2) RETURNING id",
+          [username.trim(), avatar.id]
+        );
+
+        // Use the returned user id to insert into user_avatars table
+        const insertUserAvatar = insertUser.then((user) => {
+          return t.none(
+            "INSERT INTO user_avatars(user_id, avatar_id) VALUES($1, $2)",
+            [user.id, avatar.id]
+          );
+        });
+
+        return t.batch([insertUser, insertUserAvatar]);
+      });
+
       ctx.status = 201; // Created
       ctx.body = { message: "User created successfully with avatar" };
     }
   } catch (error) {
-    ctx.status = 500; // Internal Server Error
-    ctx.body = { error: "Internal server error" };
+    // Handle unique violation or other errors
+    if (error.code === "23505") {
+      ctx.status = 409; // Conflict
+      ctx.body = { error: "Username already exists" };
+    } else {
+      ctx.status = 500; // Internal Server Error
+      ctx.body = { error: "Internal server error" };
+    }
   }
 });
 
