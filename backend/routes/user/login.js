52aac26e5a6faafba708991be0ab570b831af05e
elvijsr <erascevs@gmail.com>
2023-11-11 13:51:26 +0200

    login unique user bypass

--- backend/routes/user/login.js
+++ backend/routes/user/login.js
@@ -1,24 +1,38 @@
-// login.js
 const Router = require("koa-router");
 const db = require("../../db");
 const router = new Router();
+
+// Flag to control the behavior when the user already exists
+const returnSuccessOnExistingUser = true;
 
 router.post("/login", async (ctx) => {
   const { username } = ctx.request.body;
 
   try {
-    // Insert the new username into the users table
-    await db.none("INSERT INTO users(username) VALUES($1)", username);
-    ctx.status = 201; // Created
-    ctx.body = { message: "User created successfully" };
-  } catch (error) {
-    if (error.code === "23505") {
-      ctx.status = 409; // Conflict
-      ctx.body = { error: "Username already exists" };
+    // Check if the user already exists
+    const userExists = await db.oneOrNone(
+      "SELECT username FROM users WHERE username = $1",
+      username
+    );
+
+    if (userExists) {
+      // If returnSuccessOnExistingUser is true, return a success message
+      if (returnSuccessOnExistingUser) {
+        ctx.status = 200; // OK
+        ctx.body = { message: "Login successful" };
+      } else {
+        ctx.status = 409; // Conflict
+        ctx.body = { error: "Username already exists" };
+      }
     } else {
-      ctx.status = 500; // Internal Server Error
-      ctx.body = { error: "Internal server error" };
+      // Insert the new username into the users table
+      await db.none("INSERT INTO users(username) VALUES($1)", username);
+      ctx.status = 201; // Created
+      ctx.body = { message: "User created successfully" };
     }
+  } catch (error) {
+    ctx.status = 500; // Internal Server Error
+    ctx.body = { error: "Internal server error" };
   }
 });
 
