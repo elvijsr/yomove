7729c12d8a90cfd3628b17c1c643bc63c16e9656
elvijsr <erascevs@gmail.com>
2023-11-11 15:23:57 +0200

    challenges route update

--- backend/routes/core/challenges.js
+++ backend/routes/core/challenges.js
@@ -1,8 +1,18 @@
 const Router = require("koa-router");
+const db = require("../../db");
 const router = new Router();
 
-router.get("/challenges", (ctx, next) => {
-  ctx.body = { message: "Hello world" };
+router.get("/challenges", async (ctx, next) => {
+  try {
+    const challenges = await db.any(
+      "SELECT id, challenge_name, description, image_source FROM challenges"
+    );
+    ctx.status = 200; // OK
+    ctx.body = { challenges: challenges };
+  } catch (error) {
+    ctx.status = 500; // Internal Server Error
+    ctx.body = { error: "Internal server error" };
+  }
 });
 
 module.exports = router;
