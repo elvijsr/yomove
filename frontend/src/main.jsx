3db3f02d0a553617386ed624bab32428ad4c7f95
arturs-skutans <arturs.skutans@lokalise.com>
2023-11-11 13:13:52 +0200

    Started base theme, font installed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,47 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Layout from "./pages/Layout.jsx";
 import Profile from "./pages/Profile.jsx";
+import { Typography } from "@mui/joy";
+import { CssVarsProvider, extendTheme } from "@mui/joy/styles";
+import "@fontsource/nunito/900-italic.css";
+
+const theme = extendTheme({
+  typography: {
+    h1: {
+      background:
+        "linear-gradient(90deg, rgba(255,179,71,1) 0%, rgba(255,204,51,1) 100%);",
+      WebkitBackgroundClip: "text",
+      WebkitTextFillColor: "transparent",
+    },
+  },
+  fontFamily: {
+    display: "Nunito",
+    body: "Nunito",
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        main: {
+          secondary: "#000000",
+        },
+        gradient: {
+          primary:
+            "linear-gradient(90deg, rgba(255,179,71,1) 0%, rgba(255,204,51,1) 100%);",
+        },
+      },
+    },
+  },
+  components: {
+    JoyButton: {
+      styleOverrides: {
+        root: {
+          backgroundImage:
+            "linear-gradient(90deg, rgba(255,179,71,1) 0%, rgba(255,204,51,1) 100%)",
+        },
+      },
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -24,6 +65,14 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <CssVarsProvider theme={theme}>
+      <Typography
+        sx={{ color: theme.colorSchemes.light.palette.gradient.primary }}
+        level="h1"
+      >
+        YoMove!
+      </Typography>
+      <RouterProvider router={router} />
+    </CssVarsProvider>
   </React.StrictMode>
 );
