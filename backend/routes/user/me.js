b10a4f8c0185bdc3d7d5a923832a15b4837fc013
Rolands Gerhards <gerhards.rolands@gmail.com>
2023-11-11 16:41:22 +0200

    setavatar added

--- backend/routes/user/me.js
+++ backend/routes/user/me.js
@@ -71,4 +71,52 @@ router.get("/mypossibleavatars", async (ctx, next) => {
   }
 });
 
+router.post("/setavatar", async (ctx, next) => {
+  try {
+    // Extract the username from the request headers
+    const username = ctx.request.headers['x-username'];
+
+    // Extract the level from the request body
+    const { level } = ctx.request.body;
+
+    // Check if the username and level are provided
+    if (!username || level === undefined) {
+      ctx.status = 400;
+      ctx.body = { error: "Username and level are required" };
+      return;
+    }
+
+    // Query the database to find the user ID based on the username
+    const user = await db.oneOrNone("SELECT id FROM users WHERE username = $1", username);
+
+    // If the user is not found, return an error
+    if (!user) {
+      ctx.status = 404;
+      ctx.body = { error: "User not found" };
+      return;
+    }
+
+    // Query the database to get a random avatar ID from pictures where type equals the provided level
+    const avatar = await db.oneOrNone("SELECT id FROM pictures WHERE type = $1 ORDER BY RANDOM() LIMIT 1", level);
+
+    // If no avatar is found, return an error
+    if (!avatar) {
+      ctx.status = 404;
+      ctx.body = { error: "No avatar found for the given level" };
+      return;
+    }
+
+    // Insert a new record into user_avatars
+    await db.none("INSERT INTO user_avatars (user_id, avatar_id) VALUES ($1, $2)", [user.id, avatar.id]);
+
+    // Return a success message
+    ctx.status = 201;
+    ctx.body = { message: "Avatar set successfully" };
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { error: "Error processing request" };
+  }
+});
+
+
 module.exports = router;
