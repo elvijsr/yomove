1bd6606118d47fdd0695a061c54f6a3691ab8883
Rolands Gerhards <gerhards.rolands@gmail.com>
2023-11-12 03:05:29 +0200

    get all scores with urls

--- backend/routes/core/scores.js
+++ backend/routes/core/scores.js
@@ -5,12 +5,14 @@ const router = new Router();
 
 router.get("/getallscores", async (ctx, next) => {
   try {
-    // Query the database to get all data from results table, joined with challenges and users tables
+    // Query the database to get all data from results table, joined with challenges, users, and user_avatars tables
     const allScores = await db.manyOrNone(`
-      SELECT r.*, c.challenge_name, u.username
+      SELECT r.*, c.challenge_name, u.username,
+        (select url from pictures where id = ua.avatar_id) as avatar_scr
       FROM results r
       JOIN challenges c ON r.challenge_id = c.id
       JOIN users u ON r.user_id = u.id
+      LEFT JOIN user_avatars ua ON u.id = ua.user_id
       ORDER BY c.challenge_name, r.score DESC
     `);
 
@@ -20,13 +22,14 @@ router.get("/getallscores", async (ctx, next) => {
       if (!acc[score.challenge_name]) {
         acc[score.challenge_name] = [];
       }
-      // Add the score with the username and placement to the appropriate challenge_name group
+      // Add the score with the username, placement, user_id (as avatarSrc), and avatar_id (as avatarId) to the appropriate challenge_name group
       acc[score.challenge_name].push({
         placement: acc[score.challenge_name].length + 1, // Assign placement number
         username: score.username,
         score: score.score,
         score_date: score.score_date,
-        avatarSrc: "https://res.cloudinary.com/dpajrrxiq/image/upload/v1699700662/0-1_huhgdz.png" // Hardcoded avatar URL
+        avatarSrc: score.avatar_scr, // Use user_id as avatarSrc
+        avatarId: score.avatar_id // Add avatar_id from the user_avatars table
       });
       return acc;
     }, {});
