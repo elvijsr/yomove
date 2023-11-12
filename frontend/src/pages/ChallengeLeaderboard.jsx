699d5b02e19b59fab7688488d864e078f5733a76
Rihards Dievapeds <rihards.dievapeds@vr.fi>
2023-11-12 02:02:46 +0200

    Limiting challenge leaderboards to 15.

--- frontend/src/pages/ChallengeLeaderboard.jsx
+++ frontend/src/pages/ChallengeLeaderboard.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import List from '@mui/joy/List';
-import ListItem from '@mui/joy/ListItem';
-import Avatar from '@mui/joy/Avatar';
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import Avatar from "@mui/joy/Avatar";
 import { ListItemDecorator } from "@mui/joy";
 
 function ChallengeLeaderboard({ scores }) {
- return (
- <List>
-   {scores.map((score, index) => (
-     <ListItem key={index}>
-        {score.placement}. 
-       <ListItemDecorator>
-         <Avatar src={score.avatarSrc} />
-       </ListItemDecorator>
-        {score.username} - {score.score}
-     </ListItem>
-   ))}
- </List>
- );
+  return (
+    <List>
+      {scores.slice(0, 15).map((score, index) => (
+        <ListItem key={index}>
+          {score.placement}.
+          <ListItemDecorator>
+            <Avatar src={score.avatarSrc} />
+          </ListItemDecorator>
+          {score.username} - {score.score}
+        </ListItem>
+      ))}
+    </List>
+  );
 }
 
 export default ChallengeLeaderboard;
