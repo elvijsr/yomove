9d881197fed6ee442c687e607782f2504b3f0375
Rolands Gerhards <gerhards.rolands@gmail.com>
2023-11-12 03:10:05 +0200

    get all scores with urls with unique

--- backend/routes/core/scores.js
+++ backend/routes/core/scores.js
@@ -8,7 +8,7 @@ router.get("/getallscores", async (ctx, next) => {
     // Query the database to get all data from results table, joined with challenges, users, and user_avatars tables
     const allScores = await db.manyOrNone(`
       SELECT r.*, c.challenge_name, u.username,
-        (select url from pictures where id = ua.avatar_id) as avatar_scr
+        (SELECT url FROM pictures WHERE id = ua.avatar_id) AS avatar_src
       FROM results r
       JOIN challenges c ON r.challenge_id = c.id
       JOIN users u ON r.user_id = u.id
@@ -22,15 +22,19 @@ router.get("/getallscores", async (ctx, next) => {
       if (!acc[score.challenge_name]) {
         acc[score.challenge_name] = [];
       }
-      // Add the score with the username, placement, user_id (as avatarSrc), and avatar_id (as avatarId) to the appropriate challenge_name group
-      acc[score.challenge_name].push({
-        placement: acc[score.challenge_name].length + 1, // Assign placement number
-        username: score.username,
-        score: score.score,
-        score_date: score.score_date,
-        avatarSrc: score.avatar_scr, // Use user_id as avatarSrc
-        avatarId: score.avatar_id // Add avatar_id from the user_avatars table
-      });
+      // Check if the username already exists in the group
+      const isUsernameExists = acc[score.challenge_name].some(s => s.username === score.username);
+      if (!isUsernameExists) {
+        // Add the score with the username, placement, and avatar URL to the appropriate challenge_name group
+        acc[score.challenge_name].push({
+          placement: acc[score.challenge_name].length + 1, // Assign placement number
+          username: score.username,
+          score: score.score,
+          score_date: score.score_date,
+          avatarSrc: score.avatar_src, // URL from the pictures table
+          avatarId: score.avatar_id // avatar_id from user_avatars table
+        });
+      }
       return acc;
     }, {});
 
@@ -47,6 +51,7 @@ router.get("/getallscores", async (ctx, next) => {
     ctx.body = { error: "Error retrieving scores from the database" };
   }
 });
+
 
 
 module.exports = router;
